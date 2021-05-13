2283c802360aaaa6b1b8057caa5c9cedf441823f
Mike Bott <mabott@gmail.com>
2021-05-13 00:47:41 +0000

    minor docker-compose fixes

--- qatime.py
+++ qatime.py
@@ -26,8 +26,8 @@ BATCH_SIZE = 10
 
 listening = False
 
-logging.basicConfig(level=logging.DEBUG, format='%(message)s', datefmt='',
-                    filename=LOG_FILE, filemode='a')
+# logging.basicConfig(level=logging.DEBUG, format='%(message)s', datefmt='',
+#                     filename=LOG_FILE, filemode='a')
 
 R = redis.Redis(host='redis')
 
@@ -44,9 +44,9 @@ def extract_keyvalue(data):
     list = data.split(',')
     timestamp = list[0]
     file_path = list[8].strip('"')  # if we don't strip quotes redis escapes
-    logging.info(list)
-    logging.info(file_path)
-    logging.info(timestamp)
+    print(list)
+    print(file_path)
+    print(timestamp)
     R.set(file_path, timestamp)
 
 
@@ -54,7 +54,7 @@ def pass_message(data):
     """filters for fs_read and fs_write, returns True for those"""
     list = data.split(',')
     op_type = list[5]
-    logging.info(op_type)
+    print(op_type)
     return op_type in ['fs_read_data', 'fs_write_data', 'fs_list_directory']
 
 
@@ -62,38 +62,38 @@ def atime_setter():
     while True:
         sleep(0.1)
         keys = [R.randomkey()]
-        logging.debug(keys)
+        print(keys)
         for key in keys:
             try:
                 path = key.decode('utf-8')
             except AttributeError as e:
-                logging.info("atime_setter() got")
-                logging.info(e)
+                print("atime_setter() got")
+                print(e)
                 continue
             local_path = NFS_MOUNT + path
-            logging.debug(local_path)
+            print(local_path)
             # get atime
             value = R.get(key)
             try:
                 atime = value.decode('utf-8')
             except AttributeError as e:
-                logging.info("atime_setter() value can't be decoded")
-                logging.info(e)
+                print("atime_setter() value can't be decoded")
+                print(e)
                 continue
-            logging.debug(atime)
+            print(atime)
             try:
-                logging.info("Attempting to touch " + local_path + " with atime " + atime)
+                print("Attempting to touch " + local_path + " with atime " + atime)
                 subprocess.check_call(['touch', '-a', '-d', atime, local_path])
                 # when the above is successful, remove it from redis
                 R.delete(key)
             except subprocess.CalledProcessError as e:
-                logging.info("Failed to delete a key")
-                logging.info(e)
+                print("Failed to delete a key")
+                print(e)
             stuff = R.get(key)
             try:
-                logging.info("STUFF:" + stuff.decode('utf-8'))
+                print("STUFF:" + stuff.decode('utf-8'))
             except AttributeError as e:
-                logging.info(e)
+                print(e)
 
 
 if __name__ == "__main__":
