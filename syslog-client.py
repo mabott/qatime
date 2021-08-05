cab5a8085a5a29ab6dead78b7f332f2ff653a000
Toby Fleming <tfleming@qumulo.com>
2021-08-05 09:20:59 -0700

    Type syslog-client.py

--- syslog-client.py
+++ syslog-client.py
@@ -4,48 +4,48 @@ import argparse
 import logging
 import logging.handlers
 
-parser = argparse.ArgumentParser(__file__, description="A syslog message generator")
+from typing import Mapping
 
-parser.add_argument(
-    "--address", "-a", default="localhost", help="The syslog message recipient address"
-)
-
-parser.add_argument(
-    "--port", "-p", type=int, default=5514, help="The syslog message recipient port"
-)
-
-parser.add_argument(
-    "--level", "-l", default="DEBUG", help="The syslog message log level"
-)
-
-parser.add_argument("--message", "-m", required=True, help="The syslog message")
+LOG_LEVELS: Mapping[str, int] = {
+    "CRITICAL": logging.CRITICAL,
+    "ERROR": logging.ERROR,
+    "WARNING": logging.WARNING,
+    "INFO": logging.INFO,
+    "DEBUG": logging.DEBUG,
+}
 
 
-def string_to_level(log_level):
-    """Convert a commandline string to a proper log level
-    @param string log_level     command line log level argument
-    @return logging.LEVEL       the logging.LEVEL object to return
-    """
-    if log_level == "CRITICAL":
-        return logging.CRITICAL
-    if log_level == "ERROR":
-        return logging.ERROR
-    if log_level == "WARNING":
-        return logging.WARNING
-    if log_level == "INFO":
-        return logging.INFO
-    if log_level == "DEBUG":
-        return logging.DEBUG
-    return logging.NOTSET
+def main() -> None:
+    parser = argparse.ArgumentParser(__file__, description="A syslog message generator")
+    parser.add_argument(
+        "--address",
+        "-a",
+        default="localhost",
+        help="The syslog message recipient address",
+    )
+    parser.add_argument(
+        "--port", "-p", type=int, default=5514, help="The syslog message recipient port"
+    )
+    parser.add_argument(
+        "--level",
+        "-l",
+        default="DEBUG",
+        help="The syslog message log level",
+        choices=LOG_LEVELS.keys(),
+    )
+    parser.add_argument("--message", "-m", required=True, help="The syslog message")
 
-
-if __name__ == "__main__":
     args = parser.parse_args()
+    level = LOG_LEVELS.get(args.level, logging.NOTSET)
+
     syslogger = logging.getLogger("SyslogLogger")
-    syslogger.setLevel(string_to_level(args.level))
+    syslogger.setLevel(level)
     handler = logging.handlers.SysLogHandler(
         address=(args.address, args.port), facility=19
     )
     syslogger.addHandler(handler)
-    syslogger.log(level=string_to_level(args.level), msg=args.message)
-    # syslogger.log(args.message)
+    syslogger.log(level=level, msg=args.message)
+
+
+if __name__ == "__main__":
+    main()
